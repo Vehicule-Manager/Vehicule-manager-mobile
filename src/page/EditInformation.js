a40d3ba3e54a1140ac9b7e6a3628c93edf2db51a
mansonboy <mrdaurelthomas@gmail.com>
2023-05-24 09:07:45 +0200

    bug for fetchCarsInfo

--- src/page/EditInformation.js
+++ src/page/EditInformation.js
@@ -1,7 +1,7 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useState} from 'react';
 import {StyleSheet, TextInput, View, Button, Text, ScrollView} from 'react-native';
-import FooterNav from "../components/layout/header";
+import FooterNav from "../components/layout/footer";
 
 export default function EditInformation() {
     const [firstName, setFirstName] = useState('');
--- src/page/ProfileLeasing.js
+++ src/page/ProfileLeasing.js
@@ -1,75 +1,119 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, TextInput, View, Button, Text, Image, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, Image, Button, ScrollView } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
+import axios from 'axios';
 import FooterNav from "../components/layout/footer";
-import { API_PATH } from '@env'
+import { API_PATH } from '@env';
 
 export default function ProfileLeasing() {
     const navigation = useNavigation();
-    const [vehicleData, setVehicleData] = useState([]);
+    const [clientData, setClientData] = useState([]);
+    const [leasingVehicles, setLeasingVehicles] = useState([]);
+    const [vehiclesInfo, setVehiclesInfo] = useState([]);
+    const [carsInfo, setCarsInfos] = useState([]);
+
     const handleLogin = () => {
         navigation.navigate("ProfileVehicle");
     }
 
     useEffect(() => {
-        console.log(API_PATH);
-        fetch(`${API_PATH}leavingVehicule/1`)
-            .then(response => response.json())
-            .then(data => setVehicleData(data))
+        axios.get(`${API_PATH}client/1`)
+            .then(response => {
+                const data = response.data;
+                setClientData(data);
+            })
             .catch(error => console.error(error));
     }, []);
-    console.log(vehicleData);
 
+    useEffect(() => {
+        axios.get(`${API_PATH}leavingVehicule/client/2`)
+            .then(response => {
+                const data = response.data;
+                setLeasingVehicles(data);
+            })
+            .catch(error => console.error(error));
+    }, []);
+
+    useEffect(() => {
+        const fetchVehiclesInfo = async () => {
+            // Obtention des IDs de véhicules à partir de leasingVehicles
+            const vehicleIds = leasingVehicles.map(vehicle => vehicle.id);
+
+            // Création d'un tableau de promesses pour les requêtes individuelles
+            const vehiclePromises = vehicleIds.map(vehicleId =>
+                axios.get(`${API_PATH}vehicule/${vehicleId}`)
+            );
+
+            try {
+                // Exécution des requêtes en parallèle
+                const responses = await Promise.all(vehiclePromises);
+
+                // Obtention des données des véhicules depuis les réponses
+                const vehicleData = responses.map(response => response.data);
+                setVehiclesInfo(vehicleData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        if (leasingVehicles.length > 0) {
+            fetchVehiclesInfo();
+        }
+    }, [leasingVehicles]);
+
+    // Aucun affichage dans console
+    useEffect(() => {
+        const fetchCarsInfo = async () => {
+            // Obtention des IDs de véhicules à partir de leasingVehicles
+            const carsIds = vehiclesInfo.map(cars => cars.id_model_car);
+
+            // Création d'un tableau de promesses pour les requêtes individuelles
+            const carsPromises = carsIds.map(carsIds =>
+                axios.get(`${API_PATH}model/${carsIds}`)
+            );
+
+            try {
+                // Exécution des requêtes en parallèle
+                const responses = await Promise.all(carsPromises);
+
+                // Obtention des données des véhicules depuis les réponses
+                const carsData = responses.map(response => response.data);
+                setCarsInfos(carsData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        if (leasingVehicles.length > 0) {
+            fetchCarsInfo();
+        }
+    }, [leasingVehicles]);
+    // And modify
+    console.log(carsInfo);
     return (
         <View style={styles.container}>
             <ScrollView>
                 <View style={styles.leasingContainer}>
-                    <Text style={styles.title}>Vos véhicules</Text>
+                    <Text>{clientData.length > 0 ? clientData[0].contract : null}</Text>
+                    <Text style={styles.title}>{clientData.length > 0 ? `${clientData[0].firstname} ${clientData[0].lastname}, Vos véhicules` : null}</Text>
                     <View style={styles.containerGap}>
-                        <View>
-                            <Text style={styles.p}>Véhicule 1</Text>
-                            <Image
-                                source={{uri: 'https://i.pinimg.com/originals/dc/19/e9/dc19e9b94a372ebc21ffeb7623d5632a.png'}}
-                                style={{width: 300, height: 150}}/>
-                            <Button
-                                title="Voir le véhicule"
-                                onPress={handleLogin}
-                            />
-                        </View>
-                        <View>
-                            <Text style={styles.p}>Véhicule 2</Text>
-                            <Image
-                                source={{uri: 'https://i.pinimg.com/originals/dc/19/e9/dc19e9b94a372ebc21ffeb7623d5632a.png'}}
-                                style={{width: 300, height: 150}}/>
-                            <Button
-                                title="Voir le véhicule"
-                                onPress={handleLogin}
-                            />
-                        </View>
-                        <View>
-                            <Text style={styles.p}>Véhicule 3</Text>
-                            <Image
-                                source={{uri: 'https://i.pinimg.com/originals/dc/19/e9/dc19e9b94a372ebc21ffeb7623d5632a.png'}}
-                                style={{width: 300, height: 150}}/>
-                            <Button
-                                title="Voir le véhicule"
-                                onPress={handleLogin}
-                            />
-                        </View>
-                        <View>
-                            <Text style={styles.p}>Véhicule 4</Text>
-                            <Image
-                                source={{uri: 'https://i.pinimg.com/originals/dc/19/e9/dc19e9b94a372ebc21ffeb7623d5632a.png'}}
-                                style={{width: 300, height: 150}}/>
-                            <Button
-                                title="Voir le véhicule"
-                                onPress={handleLogin}
-                            />
-                        </View>
+                        {vehiclesInfo.length > 0 ? (
+                            vehiclesInfo.map(vehicle => (
+                                <View key={vehicle.id}>
+                                    <Text style={styles.p}>{vehicle[0].id_model_car}</Text>
+                                    <Image
+                                        source={{ uri: 'https://i.pinimg.com/originals/dc/19/e9/dc19e9b94a372ebc21ffeb7623d5632a.png' }}
+                                        style={{ width: 300, height: 150 }} />
+                                    <Button title="Voir le véhicule" onPress={handleLogin} />
+                                </View>
+                            ))
+                        ) : (
+                            <Text>Aucun véhicule en leasing trouvé.</Text>
+                        )}
                     </View>
                 </View>
             </ScrollView>
-            <FooterNav/>
+            <FooterNav />
         </View>
     );
 }
@@ -87,7 +131,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         flexDirection: 'column',
-        rowGap: 20,
+        gap: 20,  // Utilisation de 'gap' au lieu de 'rowGap' (compatible avec React Native)
     },
     title: {
         marginTop: 50,
