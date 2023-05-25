851b98aa7a3c9dd41bac45559caf8f1818488b13
MWJulien <j.fournier@mentalworks.fr>
2023-05-25 14:35:34 +0200

    feat fetch

--- src/page/Profile.js
+++ src/page/Profile.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {StyleSheet, View, Text, Pressable, ScrollView} from 'react-native';
 import {useNavigation} from "@react-navigation/native";
 import FooterNav from "../components/layout/footer";
 import Icon from 'react-native-vector-icons/FontAwesome';
+import axios from "axios";
+import { API_PATH } from '@env';
 
 export default function Profile() {
   const navigation = useNavigation();
+  const [client, setClient] = useState([]);
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${API_PATH}client/1`)
+        .then(response => {
+          const data = response.data;
+          setClient(data[0]);
+        })
+        .catch(error => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    axios.get(`${API_PATH}user/${client.id_users}`)
+        .then(response => {
+          const data = response.data;
+          setUser(data[0]);
+        })
+        .catch(error => console.error(error));
+  }, [client]);
+
+  const getMergedClient = useMemo(() => {
+    if (user) {
+      return { ...client, user: user };
+    }
+  }, [client, user]);
 
   return (
     <View style={styles.container}>
@@ -14,7 +42,10 @@ export default function Profile() {
           <View style={styles.sections}>
             <Text style={styles.sectionTitle}>Information de connexion</Text>
             <View style={styles.row}>
-              <Text style={styles.textData}>jean.neymard@gmail.com</Text>
+              <View style={styles.loginContainer}>
+                <Text style={styles.textData}> alias : {getMergedClient ? getMergedClient.user.login : ''}</Text>
+                <Text style={styles.textData}> email : {getMergedClient ? getMergedClient.user.mail : ''}</Text>
+              </View>
               <Pressable onPress={() => navigation.navigate("EditEmail")}>
                 <Icon name="user-o" color="#00aced" size={30} />
               </Pressable>
@@ -26,14 +57,13 @@ export default function Profile() {
               <Icon name="user-o" color="#00aced" size={30} />
             </Pressable>
             <View style={styles.personalDataContainer}>
-              <Text style={styles.textData}>Aled</Text>
-              <Text style={styles.textData}>Aled</Text>
-              <Text style={styles.textData}>Aled</Text>
-              <Text style={styles.textData}>Aled</Text>
-              <Text style={styles.textData}>Aled</Text>
-              <Text style={styles.textData}>Aled</Text>
-              <Text style={styles.textData}>Aled</Text>
-              <Text style={styles.textData}>Aled</Text>
+              <Text style={styles.textData}>Prénom : {getMergedClient ? getMergedClient.firstname : ''}</Text>
+              <Text style={styles.textData}>Nom : {getMergedClient ? getMergedClient.lastname : ''}</Text>
+              <Text style={styles.textData}>Civilité : {getMergedClient ? getMergedClient.civility : ''}</Text>
+              <Text style={styles.textData}>Addresse :</Text>
+              <Text style={styles.textData}>{getMergedClient ? getMergedClient.address : ''}</Text>
+              <Text style={styles.textData}>Code postal : {getMergedClient ? getMergedClient.zipCode : ''}</Text>
+              <Text style={styles.textData}>addresse optionel : {getMergedClient ? getMergedClient.optionalAddress : ''}</Text>
             </View>
           </View>
           <View style={styles.sections}>
@@ -57,6 +87,9 @@ export default function Profile() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  loginContainer: {
+    flexDirection:  'column',
   },
   sectionTitle: {
     fontSize: 24,
--- src/page/ProfileLeasing.js
+++ src/page/ProfileLeasing.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
 import { StyleSheet, View, Text, Image, Button, ScrollView } from 'react-native';
 import { useNavigation } from "@react-navigation/native";
 import axios from 'axios';
@@ -9,12 +9,10 @@ export default function ProfileLeasing() {
     const navigation = useNavigation();
     const [clientData, setClientData] = useState([]);
     const [leasingVehicles, setLeasingVehicles] = useState([]);
-    const [vehiclesInfo, setVehiclesInfo] = useState([]);
-    const [carsInfo, setCarsInfos] = useState([]);
-
-    const handleLogin = () => {
-        navigation.navigate("ProfileVehicle");
-    }
+    const [vehicle, setVehicle] = useState([]);
+    const [model, setModel] = useState([]);
+    const [brand, setBrand] = useState([]);
+    const [status, setStatus] = useState([]);
 
     useEffect(() => {
         axios.get(`${API_PATH}client/1`)
@@ -26,7 +24,7 @@ export default function ProfileLeasing() {
     }, []);
 
     useEffect(() => {
-        axios.get(`${API_PATH}leavingVehicule/client/2`)
+        axios.get(`${API_PATH}leavingVehicule/client/1`)
             .then(response => {
                 const data = response.data;
                 setLeasingVehicles(data);
@@ -37,7 +35,7 @@ export default function ProfileLeasing() {
     useEffect(() => {
         const fetchVehiclesInfo = async () => {
             // Obtention des IDs de véhicules à partir de leasingVehicles
-            const vehicleIds = leasingVehicles.map(vehicle => vehicle.id);
+            const vehicleIds = leasingVehicles.map(leasingVehicles => leasingVehicles.id);
 
             // Création d'un tableau de promesses pour les requêtes individuelles
             const vehiclePromises = vehicleIds.map(vehicleId =>
@@ -50,7 +48,7 @@ export default function ProfileLeasing() {
 
                 // Obtention des données des véhicules depuis les réponses
                 const vehicleData = responses.map(response => response.data);
-                setVehiclesInfo(vehicleData);
+                setVehicle(vehicleData);
             } catch (error) {
                 console.error(error);
             }
@@ -63,48 +61,138 @@ export default function ProfileLeasing() {
 
     // Aucun affichage dans console
     useEffect(() => {
-        const fetchCarsInfo = async () => {
+        const fetchModels = async () => {
             // Obtention des IDs de véhicules à partir de leasingVehicles
-            const carsIds = vehiclesInfo.map(cars => cars.id_model_car);
+            const modelsIds = vehicle.map(vehicle => vehicle[0].id_model_car);
 
             // Création d'un tableau de promesses pour les requêtes individuelles
-            const carsPromises = carsIds.map(carsIds =>
-                axios.get(`${API_PATH}model/${carsIds}`)
+            const modelsPromises = modelsIds.map(modelsIds =>
+                axios.get(`${API_PATH}model/${modelsIds}`)
             );
 
             try {
                 // Exécution des requêtes en parallèle
-                const responses = await Promise.all(carsPromises);
+                const responses = await Promise.all(modelsPromises);
 
                 // Obtention des données des véhicules depuis les réponses
-                const carsData = responses.map(response => response.data);
-                setCarsInfos(carsData);
+                const modelsData = responses.map(response => response.data);
+                setModel(modelsData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        if (vehicle.length > 0) {
+            fetchModels();
+        }
+    }, [vehicle]);
+
+    useEffect(() => {
+        const fetchBrands = async () => {
+            // Obtention des IDs de véhicules à partir de leasingVehicles
+            const brandsIds = model.map(models => models[0].id_brands);
+
+            // Création d'un tableau de promesses pour les requêtes individuelles
+            const brandsPromises = brandsIds.map(brandsIds =>
+                axios.get(`${API_PATH}brand/${brandsIds}`)
+            );
+
+            try {
+                // Exécution des requêtes en parallèle
+                const responses = await Promise.all(brandsPromises);
+
+                // Obtention des données des véhicules depuis les réponses
+                const brandsData = responses.map(response => response.data);
+                setBrand(brandsData);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        if (model.length > 0) {
+            fetchBrands();
+        }
+    }, [model]);
+
+    useEffect(() => {
+        const fetchStatuses = async () => {
+            // Obtention des IDs de véhicules à partir de leasingVehicles
+            const statusesIds = leasingVehicles.map(leasingVehicles => leasingVehicles.id_statuses);
+
+            // Création d'un tableau de promesses pour les requêtes individuelles
+            const statusesPromises = statusesIds.map(statusesIds =>
+                axios.get(`${API_PATH}status/${statusesIds}`)
+            );
+
+            try {
+                // Exécution des requêtes en parallèle
+                const responses = await Promise.all(statusesPromises);
+
+                // Obtention des données des véhicules depuis les réponses
+                const statusesData = responses.map(response => response.data);
+                setStatus(statusesData);
             } catch (error) {
                 console.error(error);
             }
         };
 
         if (leasingVehicles.length > 0) {
-            fetchCarsInfo();
+            fetchStatuses();
         }
     }, [leasingVehicles]);
     // And modify
-    console.log(carsInfo);
+    const getModel = useMemo(() => {
+        return model.map((model) => {
+            return model[0];
+        });
+    }, [model]);
+
+    const getBrand = useMemo(() => {
+        return brand.map((brand) => {
+            return brand[0];
+        });
+    }, [brand]);
+
+    const getLeasingVehicles = useMemo(() => {
+        return leasingVehicles.map((leasingVehicles) => {
+            return leasingVehicles;
+        });
+    }, [leasingVehicles]);
+
+    const getStatus = useMemo(() => {
+        return status.map((status) => {
+            return status[0];
+        });
+    }, [status]);
+
+    const getMergedVehicles = useMemo(() => {
+        return vehicle.map((vehicle) => {
+            const vehicleModel = getModel.find((m) => m.id === vehicle[0].id_model_car);
+            const modelBrand = getBrand.find((b) => b.id === vehicleModel.id_brands);
+            const vehicleLeasing = getLeasingVehicles.find((l) => l.id_vehicles === vehicle.id);
+            const LeasingStatus = getStatus.find((s) => s.id === vehicleLeasing.id_statuses);
+            return { ...vehicle[0], model: vehicleModel, brand: modelBrand, status: LeasingStatus };
+        });
+    }, [vehicle, getModel, getBrand, getLeasingVehicles, getStatus]);
+    console.log(getMergedVehicles)
+
     return (
         <View style={styles.container}>
             <ScrollView>
                 <View style={styles.leasingContainer}>
-                    <Text>{clientData.length > 0 ? clientData[0].contract : null}</Text>
                     <Text style={styles.title}>{clientData.length > 0 ? `${clientData[0].firstname} ${clientData[0].lastname}, Vos véhicules` : null}</Text>
                     <View style={styles.containerGap}>
-                        {vehiclesInfo.length > 0 ? (
-                            vehiclesInfo.map(vehicle => (
+                        {getMergedVehicles.length > 0 ? (
+                            getMergedVehicles.map(vehicle => (
                                 <View key={vehicle.id}>
-                                    <Text style={styles.p}>{vehicle[0].id_model_car}</Text>
-                                    <Image
-                                        source={{ uri: 'https://i.pinimg.com/originals/dc/19/e9/dc19e9b94a372ebc21ffeb7623d5632a.png' }}
+                                    <Text style={styles.p}>{ `${vehicle.brand ? vehicle.brand.name : ''} ${vehicle.model ? vehicle.model.name : ''}`}</Text>
+                                    <View style={styles.containerBubble}>
+                                        <Text style={styles.badge}>{ vehicle.status ? vehicle.status.name : ''}</Text>
+                                        <Image
+                                            source={{ uri: 'https://i.pinimg.com/originals/dc/19/e9/dc19e9b94a372ebc21ffeb7623d5632a.png' }}
                                         style={{ width: 300, height: 150 }} />
-                                    <Button title="Voir le véhicule" onPress={handleLogin} />
+                                    </View>
+                                    <Button title="Voir le véhicule" onPress={() => navigation.navigate('ProfileVehicle', { vehicleId: vehicle.id })} />
                                 </View>
                             ))
                         ) : (
@@ -131,7 +219,26 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         flexDirection: 'column',
-        gap: 20,  // Utilisation de 'gap' au lieu de 'rowGap' (compatible avec React Native)
+        marginTop: 30,
+        gap: 30,  // Utilisation de 'gap' au lieu de 'rowGap' (compatible avec React Native)
+    },
+    containerBubble: {
+        position: 'relative',
+        flexDirection: 'column',
+        paddingTop: 10,
+    },
+    badge: {
+        position: 'absolute',
+        left: 10,
+        top: 0,
+        paddingHorizontal: 5,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        zIndex: 10,
+        backgroundColor: 'rgba(255, 255, 255, 0.7)',
+        borderRadius: '50%',
+        paddingVertical: 10,
     },
     title: {
         marginTop: 50,
